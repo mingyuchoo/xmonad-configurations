685953171274fcb9e6391ac18bbf8e30e152abe5
mingyuchoo <mingyuchoo@gmail.com>
2022-07-12 21:19:07 +0900

    updated

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -67,12 +67,12 @@ myStartupHook = return ()
 myLayoutHook  = avoidStruts $ spacing 12 $ myLayout
 
 
-myLayout = tiled ||| Mirror tiled ||| Full ||| threeCol
+myLayout = Grid ||| tiled ||| threeCol
   where
     threeCol = ThreeColMid nmaster delta ratio
     tiled    = Tall nmaster delta ratio
     nmaster  = 1     -- Detfault number of windows in the master pane
-    ratio    = 1/2   -- Default proportion of screen occupied by master pane
+    ratio    = 1/3   -- Default proportion of screen occupied by master pane
     delta    = 3/100 -- Percent of screen to increment by when resizing paines
 
 
